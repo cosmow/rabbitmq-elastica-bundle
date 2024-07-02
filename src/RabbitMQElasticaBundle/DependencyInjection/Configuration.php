1f2f3b08415f4ba1c98996f03c7091ab0c87fe6f
CosmomediaCEO <93375686+CosmomediaCEO@users.noreply.github.com>
2024-07-02 09:07:40 +0200

    Update Configuration.php

--- src/RabbitMQElasticaBundle/DependencyInjection/Configuration.php
+++ src/RabbitMQElasticaBundle/DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('vadiktok_elastica');
+        $treeBuilder = new TreeBuilder('vadiktok_elastica');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
